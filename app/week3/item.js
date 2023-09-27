c00b2fbf3443b95efd78be55fc4c524a33092ace
Isaac Saffran <88598834+Saffy127@users.noreply.github.com>
2023-09-27 12:45:56 -0600

    try

--- app/week3/item.js
+++ app/week3/item.js
@@ -1,9 +1,11 @@
 const Item = ({ name, quantity, category }) => {
     return (
-      <li className="list-item">
-        <span className="text-xl font-bold">{name}</span>
-        <span className="text-lg"> Buy {quantity} in {category}</span>
-      </li>
+      <div className="bg-white p-4 m-2 rounded border">
+        <li className="list-item">
+          <span className="text-xl font-bold">{name}</span>
+          <span className="text-lg"> Buy {quantity} in {category}</span>
+        </li>
+      </div>
     );
   };
   
