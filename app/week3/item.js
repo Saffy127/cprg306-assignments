69e27c7d2ce636f6d9b717c27ef73bdc475a1d40
Isaac Saffran <88598834+Saffy127@users.noreply.github.com>
2023-09-27 12:43:18 -0600

    ayy

--- app/week3/item.js
+++ app/week3/item.js
@@ -2,11 +2,11 @@ const Item = ({ name, quantity, category }) => {
     return (
       <li className="list-item">
         <span className="text-xl font-bold">{name}</span>
-        <span className="text-lg">Buy: {quantity}</span>
-        <span className="text-lg"> in: {category}</span>
+        <span className="text-lg"> Buy {quantity} in {category}</span>
       </li>
     );
   };
+  
   
   
   export default Item;
