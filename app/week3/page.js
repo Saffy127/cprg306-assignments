485573eebe72dd53bc508ac5b3c76a39bd1d9473
Isaac Saffran <88598834+Saffy127@users.noreply.github.com>
2023-09-27 11:22:23 -0600

    styles

--- app/week3/page.js
+++ app/week3/page.js
@@ -1,13 +1,13 @@
-// /app/week3/page.js
 import ItemList from './item-list';
 
-const Page = () => {
-  return (
-    <main className="main-content">
-      <h1 className="title">Shopping List</h1>
-      <ItemList />
-    </main>
-  );
-};
+export default function Page() {
+    return (
+      <main className="bg-gray-100 text-black p-10">
+        <h1 className="text-4xl font-semibold mb-4">Shopping List</h1>
+        <div className="bg-white rounded p-4 shadow">
+          <ItemList />
+        </div>
+      </main>
+    );
+  }
 
-export default Page;
