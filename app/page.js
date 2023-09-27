5f2197622b7bb106127a55cc9e6636f61fc79aef
Isaac Saffran <88598834+Saffy127@users.noreply.github.com>
2023-09-27 13:17:40 -0600

    Update

--- app/page.js
+++ app/page.js
@@ -6,8 +6,12 @@ const Page = () => {
     <div>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
       <StudentInfo />
-      <Link href="/week2">Week 2</Link>
-      <Link href="/week3">Week 3</Link>
+      <div>
+        <Link href="/week2">Week 2</Link>
+      </div>
+      <div>
+        <Link href="/week3">Week 3</Link>
+      </div>
     </div>
   );
 };
--- app/week3/item.js
+++ app/week3/item.js
@@ -1,14 +1,15 @@
 const Item = ({ name, quantity, category }) => {
     return (
-      <div className="bg-yellow-300 p-4 m-2 rounded border border-gray-400">
-        <li className="list-item">
-          <div className="text-xl font-bold">{name}</div>
-          <div className="text-lg">Buy {quantity} in {category}</div>
-        </li>
+      <div className="bg-yellow-300 p-4 m-2 rounded border-2 border-gray-400">
+        <ul className="list-none">
+          <li className="list-item">
+            <div className="text-xl font-bold">{name}</div>
+            <div className="text-lg">Buy {quantity} in {category}</div>
+          </li>
+        </ul>
       </div>
     );
   };
-  
-  
+   
   export default Item;
   
