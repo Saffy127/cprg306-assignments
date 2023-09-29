6448a733fdde41068b0dd536a8e5da172e0b1c25
Isaac Saffran <88598834+Saffy127@users.noreply.github.com>
2023-09-29 13:24:37 -0600

    Week 4 work

--- app/page.js
+++ app/page.js
@@ -12,6 +12,9 @@ const Page = () => {
       <div>
         <Link href="/week3">Week 3</Link>
       </div>
+      <div>
+        <Link href="/week4">Week 4</Link>
+      </div>
     </div>
   );
 };
