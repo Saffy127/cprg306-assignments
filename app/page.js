eb36c427504e694b60ef8aad75320028aed820dc
Isaac Saffran <saffy127@outlook.com>
2023-11-22 18:18:34 -0700

    revert

--- app/page.js
+++ app/page.js
@@ -25,10 +25,7 @@ const Page = () => {
         <Link href="/week7">Week 7</Link>
       </div>
       <div>
-        <Link href="/week8">Week 8</Link>
-      </div>
-      <div>
-        <Link href="/week10">Week 10</Link>
+        <Link href="/week8"></Link>
       </div>
     </div>
   );
