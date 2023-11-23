b5ef0b491134bc2c84817a84cdcc473f278a0e32
Isaac Saffran <saffy127@outlook.com>
2023-11-22 18:00:33 -0700

    Update firebase.js

--- app/week8/_utils/firebase.js
+++ app/week8/_utils/firebase.js
@@ -1,8 +1,10 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore"; // For Importing FireStore
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-import { getAuth } from "firebase/auth";
  
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -16,3 +18,8 @@ const firebaseConfig = {
  
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Initialize and export Firestore
+export const db = getFirestore(app);
+
+export const auth = getAuth(app);
